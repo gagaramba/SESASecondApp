6065dae96a381ec3c9e06be6b4d25560da2ebe24
lentz <lentz@hrz.tu-chemnitz.de>
2015-01-28 03:03:18 +0100

    Set transmitted string as new text for view_message TextView

--- app/src/main/java/sesasecondapp/sse/etit/tuchemnitz/de/sesasecondapp/DisplayMessageFragment.java
+++ app/src/main/java/sesasecondapp/sse/etit/tuchemnitz/de/sesasecondapp/DisplayMessageFragment.java
@@ -2,14 +2,19 @@ package sesasecondapp.sse.etit.tuchemnitz.de.sesasecondapp;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by rlentz on 27.01.15.
  */
 public class DisplayMessageFragment extends Fragment {
+
+    private TextView viewMessage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,11 +23,26 @@ public class DisplayMessageFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.display_message_fragment, container, false);
+
+        View layout = inflater.inflate(R.layout.display_message_fragment, null);
+        viewMessage = (TextView) layout.findViewById(R.id.view_message);
+
+        return layout;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(MainActivity.LOGTAG, "Entered displaymessage");
+
+        String rcvdParameter = getArguments().getString(MainActivity.HANDOVER_PARAMETER);
+        Log.d(MainActivity.LOGTAG, "rcvdParameter: " + rcvdParameter);
+
+        if(!TextUtils.isEmpty(rcvdParameter)){
+            viewMessage.setText(rcvdParameter);
+        }
+
+
+
     }
 }
